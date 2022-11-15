a2ffabf144283da58ee5c9c07e37efa63c23e3b5
BaphometC <alex.alme@hotmail.com>
2022-11-15 15:47:06 -0500

    Cambios Agregado de Capas: Model y Controller(agregado de clases(KardexSunat y Producto)), Creacion de Formularios Kardex, implementacion de ReportViewer

---
+++ ControllerGenerador/ConnectionExcel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControllerGenerador
+{
+    public class ConnectionExcel
+    {
+        public static OleDbConnection ConexionExcel()
+        {
+            string conexionExcel = string.Format("Provider = Microsoft.ACE.OLEDB 12.0; Data Source = {0}; Extends Properties = 'Excel 12.0' ");
+            OleDbConnection connector = new OleDbConnection(conexionExcel);
+            return connector;
+
+        }
+    }
+}
